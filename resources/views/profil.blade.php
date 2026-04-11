5f08f1c1940a1ff14d49f317e08c6614325d7e64
sherlymeisya-wq <sherlymeisyamaharani@students.amikom.ac.id>
2026-04-11 22:09:44 +0700

    Update profil.blade.php

---
+++ resources/views/profil.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Profil</title>
+<script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-slate-100">
+
+<!-- NAVBAR -->
+<div class="bg-indigo-600 text-white p-4 flex gap-4 justify-center">
+    <a href="/katalog">Katalog</a>
+    <a href="/profil" class="font-bold">Profil</a>
+    <a href="/bantuan">Bantuan</a>
+    <a href="/kontak">Kontak</a>
+</div>
+
+<div class="flex items-center justify-center h-screen">
+    <h1 class="text-2xl font-bold">Halaman Profil</h1>
+</div>
+
+</body>
+</html>
