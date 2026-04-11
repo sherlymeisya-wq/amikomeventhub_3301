c19569dc7bd5d83dada24c19c494d2800c6f93e9
sherlymeisya-wq <sherlymeisyamaharani@students.amikom.ac.id>
2026-04-11 22:09:35 +0700

    Update katalog.blade.php

---
+++ resources/views/katalog.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Katalog</title>
+<script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-slate-100">
+
+<!-- NAVBAR -->
+<div class="bg-indigo-600 text-white p-4 flex gap-4 justify-center">
+    <a href="/katalog" class="font-bold">Katalog</a>
+    <a href="/profil">Profil</a>
+    <a href="/bantuan">Bantuan</a>
+    <a href="/kontak">Kontak</a>
+</div>
+
+<div class="flex items-center justify-center h-screen">
+    <h1 class="text-2xl font-bold">Halaman Katalog</h1>
+</div>
+
+</body>
+</html>
