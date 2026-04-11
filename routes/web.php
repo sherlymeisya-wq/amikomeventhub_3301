2013525f0f99c6d4e6ebaa8ad8d697f68e532593
sherlymeisya-wq <sherlymeisyamaharani@students.amikom.ac.id>
2026-04-11 21:57:38 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return ('<h1>Ini adalah Halaman Tentang Aplikasi Event Hub</h1>');
+});
+
+Route::get('/kontak', function () {
+return view('kontak');
+});
+
+Route::get('/katalog', function () {
+return view('katalog');
+});
+
+Route::get('/profil', function () {
+return view('profil');
+});
+
+Route::get('/bantuan', function () {
+return view('bantuan');
 });
