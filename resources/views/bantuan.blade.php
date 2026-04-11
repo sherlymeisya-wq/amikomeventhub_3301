0356a6b28610fe61e7ab3a769a27b24989929813
sherlymeisya-wq <sherlymeisyamaharani@students.amikom.ac.id>
2026-04-11 22:09:30 +0700

    Update bantuan.blade.php

---
+++ resources/views/bantuan.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Bantuan</title>
+<script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-slate-100">
+
+<!-- NAVBAR -->
+<div class="bg-indigo-600 text-white p-4 flex gap-4 justify-center">
+    <a href="/katalog">Katalog</a>
+    <a href="/profil">Profil</a>
+    <a href="/bantuan" class="font-bold">Bantuan</a>
+    <a href="/kontak">Kontak</a>
+</div>
+
+<div class="flex items-center justify-center h-screen">
+    <h1 class="text-2xl font-bold">Halaman Bantuan</h1>
+</div>
+
+</body>
+</html>
