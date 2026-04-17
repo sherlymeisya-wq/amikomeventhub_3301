684590c356e38b485befaea4b4333ae074262428
sherlymeisya-wq <sherlymeisyamaharani@students.amikom.ac.id>
2026-04-17 11:00:30 +0700

    Update checkout.blade.php

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -2,5 +2,8 @@
 
 @section('content')
 <h1 class="text-xl font-bold">Checkout</h1>
-<p>Isi data pembelian tiket</p>
+
+<a href="/ticket" class="bg-blue-500 text-white px-4 py-2 rounded mt-4 inline-block">
+    Lihat Tiket
+</a>
 @endsection
