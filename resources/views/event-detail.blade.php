4d413766b2b0a76f401062204dcfc3f381bf0fa7
sherlymeisya-wq <sherlymeisyamaharani@students.amikom.ac.id>
2026-04-17 09:58:02 +0700

    Create event-detail.blade.php

---
+++ resources/views/event-detail.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<h1 class="text-xl font-bold">Detail Event</h1>
+<p>Informasi lengkap event...</p>
+
+<a href="/checkout" class="bg-blue-500 text-white px-4 py-2 rounded">
+    Beli Tiket
+</a>
+@endsection
