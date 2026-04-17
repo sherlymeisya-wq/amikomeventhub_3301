76ac57a90f9d201db7025e52064409387f818222
sherlymeisya-wq <sherlymeisyamaharani@students.amikom.ac.id>
2026-04-17 11:00:42 +0700

    Update kontak.blade.php

--- resources/views/kontak.blade.php
+++ resources/views/kontak.blade.php
@@ -1,29 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Kontak</title>
-<script src="https://cdn.tailwindcss.com"></script>
-</head>
-<body class="bg-slate-100">
+@extends('layouts.app')
 
-<!-- NAVBAR -->
-<div class="bg-indigo-600 text-white p-4 flex gap-4 justify-center">
-    <a href="/katalog">Katalog</a>
-    <a href="/profil">Profil</a>
-    <a href="/bantuan">Bantuan</a>
-    <a href="/kontak" class="font-bold">Kontak</a>
-</div>
-
-<div class="flex items-center justify-center h-screen">
-    <div class="bg-white p-8 rounded-xl shadow-lg border text-center">
-        <h1 class="text-2xl font-bold mb-2">Hubungi Kami</h1>
-        <p class="mb-4">Email: admin@amikomeventhub.com</p>
-
-        <a href="/katalog" class="bg-indigo-600 text-white px-4 py-2 rounded">
-            Ke Katalog
-        </a>
-    </div>
-</div>
-
-</body>
-</html>
+@section('content')
+<h1>Halaman Kontak</h1>
+@endsection
